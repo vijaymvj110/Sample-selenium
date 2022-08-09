1b2205721d30f2178fa2d98be3e474e430ce0f84
sithsabesh <sithsabesh@sithsabesh-desktop>
2022-08-09 13:21:08 +0530

    First commit

---
+++ TestNG testcase/src/testNGPractice/basic.java
@@ -0,0 +1,23 @@
+package testNGPractice;
+
+import org.testng.annotations.Test;
+
+public class basic {
+	@Test
+public void firstTestCase() {
+	System.out.println("This is the first testcase");
+}
+	@Test
+public void secondTestCase() {
+	System.out.println("This is the second test case");
+}
+	@Test
+public void thirdTestCase() {
+	System.out.println("This is the third test case");
+}
+	@Test
+public void fourthTestCase() {
+	System.out.println("This is the fourth test case");
+}
+
+}
